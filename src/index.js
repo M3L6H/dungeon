49232146d601442c39b7d080cce52e2ce57be8b9
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2025-11-09 09:58:46 -0600

    feat: add actions bar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
+const NONE = 'none';
+const MOVE = 'move';
+const actions = [
+  MOVE,
+  NONE,
+  NONE,
+  NONE,
+  NONE,
+  NONE,
+  NONE,
+  NONE,
+  NONE,
+  NONE,
+];
+let selected = 0;
+
+const W = 10;
+const H = 8;
+const mapElt = document.getElementById('map');
+const actionsElt = document.getElementById('actions');
+
+function render() {
+}
+
+function renderActions() {
+  actions.forEach((action, i) => {
+    const actionElt = actionsElt.children[i];
+    actionElt.dataset.action = action;
+    
+    if (i === selected) {
+      actionElt.classList.add('selected');
+    } else {
+      actionElt.classList.remove('selected');
+    }
+  });
+}
+
+function createTile() {
+  const tileElt = document.createElement('div');
+  tile.classList.add('tile');
+  return tileElt;
+}
+
+function createAction() {
+  const actionElt = document.createElement('div');
+  actionElt.classList.add('action');
+  actionElt.classList.add('material-symbols-outlined');
+  return actionElt;
+}
+
 function init() {
-  const map = document.getElementById('map');
-  const x = 10;
-  const y = 8;
-  for (let i = 0; i < x * y; ++i) {
-    const tile = document.createElement('div');
-    tile.classList.add('tile');
-    map.appendChild(tile);
+  for (let i = 0; i < W * H; ++i) {
+    mapElt.appendChild(createTile());
   }
+  actions.forEach(action => actionsElt.appendChild(createAction()));
+  renderActions();
 }
 
 addEventListener('load', () => init());
