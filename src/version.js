a40298f9e23905fb95cab518e1e25e0ce133b1b5
Michael Hollingworth <8094643+M3L6H@users.noreply.github.com>
2026-01-05 18:07:43 -0600

    fix: make default version not match anything

--- src/version.js
+++ src/version.js
@@ -1,9 +1,9 @@
 const VERSION_KEY = "version";
 
-export const VERSION = "1.3.0-alpha";
+export const VERSION = "1.3.1-alpha";
 
 export function isVersionCompatible() {
-  const savedVersion = localStorage.getItem(VERSION_KEY) ?? "0.0.0";
+  const savedVersion = localStorage.getItem(VERSION_KEY) ?? "0.0.0-none";
   const [savedNumber, savedSuffix] = savedVersion.split("-");
   const [savedMajor, savedMinor, savedPatch] = savedNumber.split(".");
   const [currentNumber, currentSuffix] = VERSION.split("-");
